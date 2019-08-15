54dc4a1d5b78fa77b0b377168b6e58ed87d70929
Jonathan Scott <jonathan.scott@gmail.com>
2019-08-15 16:15:17 -0700

    switched to hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,25 @@
 import React, {useState} from 'react';
 import './App.css';
-import { connect } from 'react-redux'
+import { connect, useSelector, useDispatch } from 'react-redux'
 import {getData} from './actions'
 import Question from './components/Question'
 import jeopardy from './img/jeopardy-logo.png'
 
 function App(props) {
+  const state = useSelector(state => state)
+  const dispatch = useDispatch()
   const [result, setResult] = useState(null);
   const handleClick = (e) => {
     e.preventDefault()
+    dispatch(getData())
     setResult(null)
-    props.getData()
 
   }
+  // 'props' changed to state for hooks
   return (
     <div className="App">
       <img style={{margin: '0 auto'}} src={jeopardy} alt='jeopardy logo'  height='272' width='640'/>
-      {props.quiz[0] && <Question quiz={props.quiz[0]} setResult={setResult} result={result} />}
+      {state.quiz[0] && <Question quiz={state.quiz[0]} setResult={setResult} result={result} />}
       <button className='btn' onClick={handleClick}>Get Question</button>
     </div>
   );
@@ -28,4 +31,5 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, { getData })(App);
+// export default connect(mapStateToProps, { getData })(App);
+export default App;
