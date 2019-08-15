39a94caa3d779c9774447efce4c45965e06998b3
Jonathan Scott <jonathan.scott@gmail.com>
2019-08-15 12:41:27 -0700

    store and actions hooked up to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-
 import './App.css';
+import { connect } from 'react-redux'
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      
+      <h1>{props.test}</h1>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    test: state.test
+  }
+}
+
+export default connect(mapStateToProps, {})(App);
---
+++ client/src/reducers/index.js
@@ -0,0 +1,11 @@
+const initialState = {
+    test: 'we made it',
+    error: "",
+    isFetching: false
+  };
+
+  export const reducer = (state = initialState, action) => {
+      switch(action.type) {
+          default: return state;
+      }
+  }
